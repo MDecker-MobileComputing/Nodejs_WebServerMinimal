5436a8412d2a59ddfe60dd1bde7cbbfb81d26433
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-02-12 15:16:21 +0100

    Server erkennt jetzt Name der angeforderten Ressource

--- src/main.js
+++ src/main.js
@@ -9,12 +9,37 @@ function fehlerBehandeln(fehlerObjekt) {
 
     if (fehlerObjekt.code === "ECONNRESET") {
 
-        console.log("Client hat Verbindung geschlossen\n");
+        console.log( "Client hat Verbindung geschlossen.\n" );
 
     } else {
 
         console.error( `Fehler: ${fehlerObjekt.message}\n` );
     }
+}
+
+
+/**
+ * Funktion liefert die Token der ersten Zeile eines HTTP-Requests
+ * als Array zurück, also v.a. HTTP-Verb und den Pfad zur Ressource.
+ * <br>
+ *
+ * Beispiel für erste Zeile im HTTP-Request:
+ * ```
+ * GET /pfad/seite.html HTTP/1.1
+ * ```
+ *
+ * @param {*} daten Von HTTP-Client über TCP/IP erhaltener Request
+ * @returns Array mit Komponenten der ersten Zeile im HTTP-Request:
+ *          HTTP-Verb (z.B. `GET`),
+ *          Pfad (Request URI, z.B. `/pfad/seite.html`),
+ *          Protokoll (z.B. `HTTP/1.1`)
+ */
+function extrahiereRequestZeile(daten) {
+
+    const zeilenArray = daten.split("\n");
+    const ersteZeile  = zeilenArray[0];
+
+    return ersteZeile.split(" ");
 }
 
 
@@ -31,12 +56,22 @@ let server = net.createServer(socket => {
 
         console.log("HTTP-Request empfangen: ", daten);
 
+        const requestTokenArray = extrahiereRequestZeile(daten);
+        const httpVerb = requestTokenArray[0];
+        if (httpVerb !== "GET") {
+
+            console.log(`FEHLER: HTTP-Verb \"${httpVerb}\" wird nicht unterstützt.`);
+        }
+        const pfadZuRessource = requestTokenArray[1];
+
         socket.write("HTTP/1.1 200 OK"         + HTTP_ZEILENENDE);
         socket.write("Content-Type: text/html" + HTTP_ZEILENENDE);
 
         socket.write(HTTP_ZEILENENDE); // Leerzeile zwischen Header und Body
 
-        socket.write("<html><body><h1>Hallo Browser!</h1></body></html>");
+        socket.write("<html><body>"                                             + HTTP_ZEILENENDE);
+        socket.write(`<h1>Datei/Ressource angefordert: ${pfadZuRessource}</h1>` + HTTP_ZEILENENDE);
+        socket.write("</body></html>"                                           + HTTP_ZEILENENDE);
 
         socket.end();
         console.log(`HTTP-Request von ${socket.remoteAddress} beantwortet.`);
@@ -46,5 +81,7 @@ let server = net.createServer(socket => {
     socket.on("error", fehlerBehandeln );
 });
 
+
+// Server starten
 console.log(`Web-Server lauscht auf Port ${PORT_NUMMER} ...\n`);
 server.listen(PORT_NUMMER, "");
